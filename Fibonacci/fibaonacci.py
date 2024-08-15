e03293c4cfd2c8ebd4a238aabaf877e7c908cf72
Robin Ramdin <robinramdin@hotmail.com>
2024-08-15 21:12:36 +0800

    updated fibaonacci.py

--- Fibonacci/fibaonacci.py
+++ Fibonacci/fibaonacci.py
@@ -4,21 +4,21 @@ def main(n:int)-> None:
 
     Args:
         n (int): nth term in fibonacci 
-    """    
+    """   
+    n_1, n_2 = 0, 1 # instantiate the first two terms 
     # base case 
     if n <= 1: 
-        return n
+        if n == 1: return n_2
+        if n == 0: return n_1
     else: 
-        n_1, n_2 = 0, 1 # instantiate the first two terms
         iter = 0 # set the iteration variable to 0
         while iter < n: 
             temp_n = n_2 # temporarily save the n_2 term
             n_2 = n_1 + n_2 # compute new term
             n_1 = temp_n # assign saved term to n_1
             iter += 1 # increment the iteration by 1
-        print(n_2)
-    return    
+    return n_1
 
 if __name__ == '__main__':
     n = input()
-    main(int(n))
+    print(main(int(n)))
